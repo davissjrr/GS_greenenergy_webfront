f81fec36372794020480b0e505de9ce366cbba05
Witalon Rodrigues <162512211+witalonrodrigues@users.noreply.github.com>
2024-11-22 10:36:09 -0300

    feat - página de cadastro com js, html e css

---
+++ cadastro.js
@@ -0,0 +1,24 @@
+const senha = document.querySelector('#senha');
+const usuario = document.querySelector('#usuario');
+const senhaAgain = document.querySelector('#repita-senha');
+const cadastrar = document.querySelector('.btn-logar');
+
+cadastrar.addEventListener('click', () => {
+    const usuarioValor = usuario.value.trim();
+    const senhaValor = senha.value.trim();
+    const senhaAgainValor = senhaAgain.value.trim();
+
+
+    if (!usuarioValor || !senhaValor || !senhaAgainValor) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    if (senhaValor !== senhaAgainValor) {
+        alert('As senhas não coincidem. Por favor, tente novamente.');
+        return;
+    }
+
+    alert(`Cadastro realizado com sucesso!\nUsuário: ${usuarioValor}`);
+    window.location.href = 'login.html'
+});
